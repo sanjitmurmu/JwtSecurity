c24f4751dee63c097e26daf72a62320c7b8b6bea
murmu <91903@LAPTOP-O8NT12LD>
2023-09-13 21:37:57 +0530

    Initial commit.

---
+++ JwtSecurityLearn/src/main/java/com/security/jwt/learn/JwtSecurityLearn/JwtSecurityLearnApplication.java
@@ -0,0 +1,13 @@
+package com.security.jwt.learn.JwtSecurityLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecurityLearnApplication.class, args);
+	}
+
+}
---
+++ JwtSecurityLearn/src/test/java/com/security/jwt/learn/JwtSecurityLearn/JwtSecurityLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.jwt.learn.JwtSecurityLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurityLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
